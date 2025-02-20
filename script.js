4623629a4ba1b808996f1dc4609bbdaeb8e4b331
Barbara Griffin <bag155@miami.edu>
2025-02-20 13:23:35 -0500

    updating index

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
 let questionNumber = 0
 
+function nextQuestion(){
+    let questions = document.querySelectorAll(".question");
+    questions.forEach(function (question,index){
+        console.log(index);
+        if (index == questionNumber){
+            question.hidden = false;
+        }else{
+            question.hidden = true;
+        }
+       
+    });
+
+
+
+}
+
 
 function startGame(){
-    let startButton = document.querySelector("#startButton")
-    let meow = document.querySelector('#startMeow')
+    let meow = document.querySelector("#startMeow")
+
+    let Start = document.querySelector("#start")
     console.log("start!")
-    startButton.hidden = true;
+    Start.hidden = true;
     meow.play();
     setTimeout(nextQuestion, 2000);
     
 }
 
 function gameOver(){
+        let gameoverSound = document.querySelector("#gameOverSound")
         let gameover = document.querySelector("#gameOverscreen")
         gameover.hidden = false;
+        gameoverSound.play();
         console.log("you die :(")
         
 }
 
 function Win(){
-    let winscreen = document.querySelector("completedScreen")
+    let winscreen = document.querySelector("#completedScreen")
     winscreen.hidden = false
 }
 function restart(){
-    startButton.hidden = false;
+    start.hidden = false;
     gameOverscreen.hidden = true;
     heartNumber.textContent = 3;
     questionNumber = 0;
@@ -40,25 +59,6 @@ function disableButtons(question){
 
 
 
-function nextQuestion(){
-    let questions = document.querySelectorAll('.question');
-    questionNumber++;
-    questions.forEach(function (question,index){
-        console.log(index);
-        if (index == questionNumber){
-            question.hidden = false;
-        }else{
-            question.hidden = true;
-        }
-       
-    });
-
-
-
-}
-
-
-
 function check(button){
     let question = button.closest('.question')
     let right = document.querySelector("#right")
@@ -67,7 +67,7 @@ function check(button){
     console.log(button)
 
     if(button.classList.contains("correct")){
-        
+        questionNumber++;
         button.style.background = "green";
         button.style.color = "white"
         disableButtons(question);
@@ -84,7 +84,7 @@ function check(button){
 
     }
 
-    else if (questionNumber == 12){
+    else if (questionNumber > 12){
         Win();
         question.hidden = true;
     }
